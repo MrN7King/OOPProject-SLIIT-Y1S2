c739d50192b0e24427bb39acce734d71bee7b0e1
V.Gurunivasan <155348464+MrN7King@users.noreply.github.com>
2024-06-26 16:32:27 +0530

    Add files via upload

---
+++ OOPFinalProject/src/main/java/Views/ManageCustomerOrderView.java
@@ -0,0 +1,15 @@
+package Views;
+
+import javax.swing.*;
+
+public class ManageCustomerOrderView {
+    private JScrollPane CustomerTable;
+    private JTable table1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JCheckBox sparePartsCheckBox;
+    private JCheckBox repaintingCheckBox;
+    private JCheckBox repairsCheckBox;
+    private JButton AddCustomerButton;
+}
