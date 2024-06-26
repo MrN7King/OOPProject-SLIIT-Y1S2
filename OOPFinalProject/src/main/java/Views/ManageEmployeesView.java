4bcc781525dd99a7d6f7d786a8bc4736dd89a6e5
Dilaksan <159269725+Dilaksan2005@users.noreply.github.com>
2024-06-26 21:58:45 +0530

    New Files
    Has all the views

---
+++ OOPFinalProject/src/main/java/Views/ManageEmployeesView.java
@@ -0,0 +1,11 @@
+package Views;
+
+import javax.swing.*;
+
+public class ManageEmployeesView {
+    private JButton getAllEmployeesButton;
+    private JButton getOfficeEmployeesButton;
+    private JButton getShipEmployeesButton;
+    private JButton getFieldEmployeesButton;
+    private JTable table1;
+}
